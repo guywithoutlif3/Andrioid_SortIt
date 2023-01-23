c09995daae27f256e02d00563b2eb57a3f21ca6a
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2023-01-23 10:16:40 +0100

    started multi threading

--- app/src/main/java/com/example/sortit/GameActivity.java
+++ app/src/main/java/com/example/sortit/GameActivity.java
@@ -30,6 +30,9 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.*;
@@ -50,7 +53,8 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor gyro;
     DisplayMetrics displayMetrics = new DisplayMetrics();
-
+    private Executor executor = Executors.newFixedThreadPool(4);
+    CountDownLatch latch = new CountDownLatch(1);
     int score;
     int highscore;
 
@@ -84,7 +88,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
 
     //responsible for spawning a new image in the middle of the screen upon game launch and
     //when a new image is supposed to be spawned when scorred
-    public void spawn() {
+    public void spawn() throws InterruptedException {
         //get the gameLayout from View
         LinearLayout layout = (LinearLayout) findViewById(R.id.gamelayout);
         //get the image where the image get inserted / spawned into from View
@@ -101,6 +105,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
                 ivBasicImage.setAccessibilityPaneTitle("criminal");
             }
             //get the ImageMatrix / data of the url and right after load it into the image on screen
+            latch.await();
             Picasso.get().load(criminals[randArrVal]).into(ivBasicImage);
             ivBasicImage.refreshDrawableState();
             layout.refreshDrawableState();
@@ -123,6 +128,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
                 ivBasicImage.setAccessibilityPaneTitle("stock");
             }
             //get the ImageMatrix / data of the url and right after load it into the image on screen
+            latch.await();
             Picasso.get().load(stockPeople[randArrVal]).into(ivBasicImage);            // Adds the view to the layout
             ivBasicImage.refreshDrawableState();
             layout.refreshDrawableState();
@@ -142,73 +148,103 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+
+
+
+
     //get all the images thru the jailbase API and sort them into their array "crimnials"
     public void getCriminals() throws IOException, JSONException {
-        //make Policy permit all
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                .permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        // New okHttp client
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        // set media type of querry
-        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
-        // build the request
-        Request request = new Request.Builder()
-                .url("https://jailbase-jailbase.p.rapidapi.com/recent/?source_id=ar-jcso")
-                .method("GET", null)
-                .addHeader("X-RapidAPI-Key", "e451035421msh38c089d44761c42p1b4070jsn3ebb2f8a409c")
-                .addHeader("X-RapidAPI-Host", "jailbase-jailbase.p.rapidapi.com")
-                .build();
-        // execute the query
-        Response response = client.newCall(request).execute();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                //make Policy permit all
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                // New okHttp client
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                        .build();
+                // set media type of querry
+                MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+                // build the request
+                Request request = new Request.Builder()
+                        .url("https://jailbase-jailbase.p.rapidapi.com/recent/?source_id=ar-jcso")
+                        .method("GET", null)
+                        .addHeader("X-RapidAPI-Key", "e451035421msh38c089d44761c42p1b4070jsn3ebb2f8a409c")
+                        .addHeader("X-RapidAPI-Host", "jailbase-jailbase.p.rapidapi.com")
+                        .build();
+                // execute the query
+                Response response = client.newCall(request).execute();
 
-        // part bellow goes into the JSON Object and sorts the images urls into their array
-        JSONObject Jobject = new JSONObject(response.body().string());
-        JSONArray Jarray = Jobject.getJSONArray("records");
+                // part bellow goes into the JSON Object and sorts the images urls into their array
+                JSONObject Jobject = new JSONObject(response.body().string());
+                JSONArray Jarray = Jobject.getJSONArray("records");
 
-        for (int i = 0; i < Jarray.length(); i++) {
-            JSONObject object = Jarray.getJSONObject(i);
-            criminals = Arrays.copyOf(criminals, criminals.length + 1);
-            criminals[criminals.length - 1] = object.getString("mugshot");
-            System.out.println(object.getString("mugshot"));
-        }
+                for (int i = 0; i < Jarray.length(); i++) {
+                    JSONObject object = Jarray.getJSONObject(i);
+                    criminals = Arrays.copyOf(criminals, criminals.length + 1);
+                    criminals[criminals.length - 1] = object.getString("mugshot");
+                    System.out.println(object.getString("mugshot"));
+                }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
     }
 
     //get all the images thru the pexels API and sort them into their array "stockPeople"
     public void getStockPeople() throws IOException, JSONException {
-        //make Policy permit all
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                .permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        // build the request
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        // set media type of querry
-        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
-        // build the request
-        Request request = new Request.Builder()
-                .url("https://pexelsdimasv1.p.rapidapi.com/v1/search?query=mugshot&locale=en-US&per_page=2")
-                .method("GET", null)
-                .addHeader("Authorization", "563492ad6f91700001000001ddd8be82192b429ab31f5adc64661679")
-                .addHeader("X-RapidAPI-Key", "e451035421msh38c089d44761c42p1b4070jsn3ebb2f8a409c")
-                .addHeader("X-RapidAPI-Host", "PexelsdimasV1.p.rapidapi.com")
-                .build();
-        // execute the query
-        Response response = client.newCall(request).execute();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                // Make API call here
+                //make Policy permit all
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                // build the request
+                OkHttpClient client = new OkHttpClient().newBuilder()
+                        .build();
+                // set media type of querry
+                MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+                // build the request
+                Request request = new Request.Builder()
+                        .url("https://pexelsdimasv1.p.rapidapi.com/v1/search?query=mugshot&locale=en-US&per_page=2")
+                        .method("GET", null)
+                        .addHeader("Authorization", "563492ad6f91700001000001ddd8be82192b429ab31f5adc64661679")
+                        .addHeader("X-RapidAPI-Key", "e451035421msh38c089d44761c42p1b4070jsn3ebb2f8a409c")
+                        .addHeader("X-RapidAPI-Host", "PexelsdimasV1.p.rapidapi.com")
+                        .build();
+                // execute the query
+                Response response = client.newCall(request).execute();
 
-        // part bellow goes into the JSON Object and sorts the images urls into their array
-        JSONObject Jobject = new JSONObject(response.body().string());
-        JSONArray Jarray = Jobject.getJSONArray("photos");
-        for (int i = 0; i < Jarray.length(); i++) {
-            JSONObject object = Jarray.getJSONObject(i);
-            System.out.println(object.toString());
+                // part bellow goes into the JSON Object and sorts the images urls into their array
+                JSONObject Jobject = new JSONObject(response.body().string());
+                JSONArray Jarray = Jobject.getJSONArray("photos");
+                for (int i = 0; i < Jarray.length(); i++) {
+                    JSONObject object = Jarray.getJSONObject(i);
+                    System.out.println(object.toString());
 
-            stockPeople = Arrays.copyOf(stockPeople, stockPeople.length + 1);
-            String srcString = object.getJSONObject("src").getString("tiny");
-            stockPeople[stockPeople.length - 1] = srcString;
-            System.out.println(srcString);
-        }
+                    stockPeople = Arrays.copyOf(stockPeople, stockPeople.length + 1);
+                    String srcString = object.getJSONObject("src").getString("tiny");
+                    stockPeople[stockPeople.length - 1] = srcString;
+                    System.out.println(srcString);
+                }
+            }catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+        }});
+
     }
 
     public void getCats()
@@ -283,22 +319,39 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
         // i call  the function checkCategory which is responsible for checking if correct or not
         if (ivBasicImage.getY() > height) {
             System.out.println("down");
-            checkCategory("down");
+            try {
+                checkCategory("down");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } else if (ivBasicImage.getY() <= 0) {
             System.out.println("up");
-            checkCategory("up");
+            try {
+                checkCategory("up");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } else if (ivBasicImage.getX() > width) {
             System.out.println("right");
-            checkCategory("right");
+            try {
+                checkCategory("right");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         } else if (ivBasicImage.getX() <= 0) {
             System.out.println("left");
-            checkCategory("left");
+            try {
+                checkCategory("left");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
+
     // This function is responsible for checking the direction of the moved image and act accordingly
-    public void checkCategory(String direction) {
+    public void checkCategory(String direction) throws InterruptedException {
         // get image to set X and Y
         ImageView ivBasicImage = (ImageView) findViewById(R.id.dumbFuckingPicture);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
@@ -334,6 +387,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
 
         }
     }
+
     // this function says what it does: It adds 1 to score and gets called if right
     public void addToScore() {
         final TextView scoreOnView = this.<TextView>findViewById(R.id.Score);
@@ -344,6 +398,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
         }
 
     }
+
     //Makes Player loose and send them to game over sceen / activity with theit score and highscore
     public void gameOver() {
         Intent i = new Intent(GameActivity.this, GameOver.class); // new intent
@@ -356,6 +411,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
     //here the countdown starts and starts counting down from 30 seconds
     // onFinish gameOver gets called again
     public void gameOverByTimeout() {
@@ -365,6 +421,7 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
             public void onTick(long ms) {
                 countdownOnView.setText("countdown: " + ms / 1000); // setting time left on View
             }
+
             public void onFinish() {
                 gameOver();
             }
@@ -389,19 +446,18 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
 
         super.onResume();
 
-        try {
-            getCriminals(); // call the api
-            getStockPeople(); // cal the api
-            //getDogs();
-            //getCats();
+        makeApiCall();
+        //getCriminals(); // call the api
+        //getStockPeople(); // cal the api
+        //getDogs();
+        //getCats();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            System.out.println(e);
+
+        try {
+            spawn();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        spawn();
         // On pause we register a listener for the gyro sensor
         sensorManager.registerListener(this, gyro, SensorManager.SENSOR_DELAY_NORMAL);
     }
